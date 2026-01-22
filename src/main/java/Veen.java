c43db85a0de9b930c4ec7d27f73b77579e1b2ec6
Rajan Praveen <praveenrajan70@gmail.com>
2026-01-22 15:18:02 +0800

    Rename project to Veen, update logo and change the greetings

---
+++ src/main/java/Veen.java
@@ -0,0 +1,17 @@
+public class Veen {
+    public static void main (String[] args) {
+        String logo = " __     __  ________  ________  __    __ \n"
+                + "|  \\   |  \\|        \\|        \\|  \\  |  \\\n"
+                + "| \\\\   | \\\\| $$$$$$$$| $$$$$$$$| $$\\ | $$\n"
+                + "| $$   | $$| $$__    | $$__    | $$$\\| $$\n"
+                + " \\$$\\ /  $$| $$  \\   | $$  \\   | $$$$\\ $$\n"
+                + "  \\$$\\  $$ | $$$$$   | $$$$$   | $$ \\$$$$\n"
+                + "   \\$$ $$  | $$_____ | $$_____ | $$  \\$$$\n"
+                + "    \\$$$   | $$     \\| $$     \\| $$   \\$$\n"
+                + "     \\$     \\$$$$$$$$ \\$$$$$$$$ \\$$    \\$$\n";
+        System.out.println(logo);
+        System.out.println("Hello! I'm VEEN");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
