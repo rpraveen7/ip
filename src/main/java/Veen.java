ad1c15695ac64b3f3692f14c9675f843cd44d12b
Rajan Praveen <praveenrajan70@gmail.com>
2026-01-22 21:38:10 +0800

    Added Horizontal Lines

--- src/main/java/Veen.java
+++ src/main/java/Veen.java
@@ -1,8 +1,11 @@
 public class Veen {
     public static void main (String[] args) {
 
-        System.out.println("Hello! I'm VEEN");
+        System.out.println("_______________________________________");
+        System.out.println("Hello! I'm Veen");
         System.out.println("What can I do for you?");
+        System.out.println("_______________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_______________________________________");
     }
 }
