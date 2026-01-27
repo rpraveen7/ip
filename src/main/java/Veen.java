1cdffa24d0bfe3b342d031d6fb4610c032b1e5fa
rpraveen7 <praveenrajan70@gmail.com>
2026-01-27 16:55:31 +0800

    Level-1. Echo function

--- src/main/java/Veen.java
+++ src/main/java/Veen.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Veen {
     public static void main (String[] args) {
 
         System.out.println("_______________________________________");
         System.out.println("Hello! I'm Veen");
-        System.out.println("What can I do for you?");
+        System.out.println("Yo! What can I do for you?");
         System.out.println("_______________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        String exit = "bye";
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals(exit)) {
+            echoInput("    " + line);
+            System.out.println("_______________________________________");
+            line = in.nextLine();
+        }
         System.out.println("_______________________________________");
+        System.out.println("      Bye. Hope to see you again soon!");
+        System.out.println("_______________________________________");
+
+    }
+
+    public static void echoInput(String line) {
+        System.out.println("_______________________________________");
+        System.out.println(line);
     }
 }
