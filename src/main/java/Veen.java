d20f2d3247a0e49917c37958454e46eed985ddf7
rpraveen7 <praveenrajan70@gmail.com>
2026-01-27 17:40:49 +0800

    Level-2. Add, List functions

--- src/main/java/Veen.java
+++ src/main/java/Veen.java
@@ -4,25 +4,35 @@ public class Veen {
 
     private static final String Divider = "_______________________________________";
     private static final String Exit_Command = "bye";
+    private static final String List_Command = "list";
+    public static String[] taskList = new String[100];
+    public static int totalTasks = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         printWelcomeMessage();
 
+
         String input = scanner.nextLine();
         while (!input.equals(Exit_Command)) {
-            echoInput(input);
+            if(input.equals(List_Command)) {
+                printTasks();
+            } else {
+                echoInput(input);
+                storeTasks(input);
+                totalTasks++;
+            }
             input = scanner.nextLine();
         }
 
         printGoodbyeMessage();
         scanner.close(); //good practice
     }
-    
+
     private static void printWelcomeMessage() {
         System.out.println(Divider);
-        System.out.println("Hello! I'm Veen");
+        System.out.println("Yo! I'm Veen");
         System.out.println("What can I do for you?");
         System.out.println(Divider);
     }
@@ -35,7 +45,20 @@ public class Veen {
 
     private static void echoInput(String input) {
         System.out.println(Divider);
-        System.out.println(input);
+        System.out.println("added: " + input);
+        System.out.println(Divider);
+    }
+
+    private static void storeTasks(String input) {
+        taskList[totalTasks] = input;
+    }
+
+    private static void printTasks() {
+        System.out.println(Divider);
+        for(int i = 0; i < totalTasks; i++) {
+            System.out.print(i + 1 + ".");
+            System.out.println(" " + taskList[i]);
+        }
         System.out.println(Divider);
     }
 }
