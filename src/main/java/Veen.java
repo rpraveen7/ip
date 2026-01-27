56945d5069e0a505b60adc54b39a35c511f165e6
rpraveen7 <praveenrajan70@gmail.com>
2026-01-27 17:11:57 +0800

    Cleaned up messy code

--- src/main/java/Veen.java
+++ src/main/java/Veen.java
@@ -1,29 +1,41 @@
 import java.util.Scanner;
 
 public class Veen {
-    public static void main (String[] args) {
 
-        System.out.println("_______________________________________");
-        System.out.println("Hello! I'm Veen");
-        System.out.println("Yo! What can I do for you?");
-        System.out.println("_______________________________________");
-        String exit = "bye";
-        String line;
-        Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        while(!line.equals(exit)) {
-            echoInput("    " + line);
-            System.out.println("_______________________________________");
-            line = in.nextLine();
+    private static final String Divider = "_______________________________________";
+    private static final String Exit_Command = "bye";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        printWelcomeMessage();
+
+        String input = scanner.nextLine();
+        while (!input.equals(Exit_Command)) {
+            echoInput(input);
+            input = scanner.nextLine();
         }
-        System.out.println("_______________________________________");
-        System.out.println("      Bye. Hope to see you again soon!");
-        System.out.println("_______________________________________");
 
+        printGoodbyeMessage();
+        scanner.close(); //good practice
+    }
+    
+    private static void printWelcomeMessage() {
+        System.out.println(Divider);
+        System.out.println("Hello! I'm Veen");
+        System.out.println("What can I do for you?");
+        System.out.println(Divider);
     }
 
-    public static void echoInput(String line) {
-        System.out.println("_______________________________________");
-        System.out.println(line);
+    private static void printGoodbyeMessage() {
+        System.out.println(Divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(Divider);
+    }
+
+    private static void echoInput(String input) {
+        System.out.println(Divider);
+        System.out.println(input);
+        System.out.println(Divider);
     }
 }
